606fdd434fcd0ea32fcba94c5701e0689ce23d88
Unknown Mortal <104091630+UnknownMortal@users.noreply.github.com>
2022-06-16 18:41:44 +0530

    Update callback.py

--- robot/plugins/callback.py
+++ robot/plugins/callback.py
@@ -1,2 +1,8 @@
 from pyrogram import Client, filters
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+
+
+@Client.on_callback_query(filters.regex("cbhome"))
+async def cbhome(_, query: CallbackQuery):
+    await query.edit_message_text(
+        f"""
